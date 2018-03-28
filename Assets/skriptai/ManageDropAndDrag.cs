cd867c150ecc1313ed9c0cc578c43112c83f9e1b
SC\ginmor1 <gintare.morkunait@gmail.com>
2018-03-28 19:05:01 +0300

    kuriamas antras lygis, dar nebaigtas

---
+++ Assets/skriptai/ManageDropAndDrag.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageDropAndDrag : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Drag()
+    {
+        GameObject.Find("Image0").transform.position = Input.mousePosition;
+    }
+
+    public void Drop()
+    {
+        GameObject ph1 = GameObject.Find("A3");
+        float distance = Vector3.Distance(GameObject.Find("Image0").transform.position, ph1.transform.position);
+        if (distance < 50)
+        {
+            GameObject.Find("Image0").transform.position = ph1.transform.position;
+        }
+    }
+}
---
+++ Assets/skriptai/pirm.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pirm : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Drag1()
+    {
+        GameObject.Find("Image5").transform.position = Input.mousePosition;
+    }
+
+    public void Drop1()
+    {
+        GameObject ph1 = GameObject.Find("A1");
+        float distance = Vector3.Distance(GameObject.Find("Image5").transform.position, ph1.transform.position);
+        if (distance < 50)
+        {
+            GameObject.Find("Image5").transform.position = ph1.transform.position;
+        }
+    }
+}
