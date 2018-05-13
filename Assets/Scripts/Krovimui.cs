1e76b8dcbdcf82c11fa310321c96638bf3721270
SC\ginmor1 <gintare.morkunait@gmail.com>
2018-05-13 19:37:22 +0300

    Sutvarkytas meniu, lygiai, pirmas lygis

---
+++ Assets/Scripts/Krovimui.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Krovimui : MonoBehaviour
+{
+
+    [SerializeField]
+    private float units;
+
+    [SerializeField]
+    private GameObject createPrefab;
+
+    [SerializeField]
+    private Image fill;
+
+    private float fillAmount;
+
+    void Start()
+    {
+        StartCoroutine(BuildUnits());
+            }
+    void Update()
+    {
+        UpdateBar();
+    }
+
+    public IEnumerator BuildUnits()
+    {
+        Vector2 topLeft = Camera.main.ViewportToWorldPoint(new Vector2(0, 1));
+        Vector2 bottomRight = Camera.main.ViewportToWorldPoint(new Vector2(1, 0));
+
+        for(int i = 0; i <= units; i++)
+        {
+            Vector2 randomPos = new Vector2(Random.Range(topLeft.x, bottomRight.x), Random.Range(topLeft.y, bottomRight.y));
+
+            fillAmount = i / units;
+            Instantiate(createPrefab, randomPos, Quaternion.identity);
+            yield return null;
+        }
+        SceneManager.LoadScene("scena1");
+        
+    }
+    private void UpdateBar()
+    {
+        fill.fillAmount = fillAmount;
+    }
+}
---
+++ Assets/Scripts/MeniuKontroliavimas.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MeniuKontroliavimas : MonoBehaviour {
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("snlakn");
+        Application.Quit();
+    }
+
+    public GameObject play;
+    public Slider slider;
+
+     public void Start()
+    {
+        play = GameObject.Find("play");
+    }
+
+    public void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "playgame")
+                {
+
+                }
+            }
+        }
+    }
+
+}
